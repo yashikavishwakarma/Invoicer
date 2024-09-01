39a1950d5146c056847da536877a55391c6c3cf8
Ayaan-Ali-Khan <aliayaan201004@gmail.com>
2024-09-01 14:50:44 +0530

    ninth

--- src/buttons/LogOut.js
+++ src/buttons/LogOut.js
@@ -11,7 +11,7 @@ export default function LogOut() {
               <button className="p-[3px] relative" onClick={() => setShowLogoutModal(true)}>
                 <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
                 <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-                  Lit up borders
+                  Logout
                 </div>
               </button>
 
